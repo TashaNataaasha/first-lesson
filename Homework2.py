a4be779e940c98cd82225127dd090928e2f71b8d
Nataliia <tasha.zirka@gmail.com>
2023-05-30 15:10:39 +0300

    added task1

---
+++ Homework2.py
@@ -0,0 +1,16 @@
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+# num3 = int(input("Введите третье число: "))
+# user_choice = int(input("Введите 1 - max, 2 - min, 3 - mean: "))
+# max = max(num1, num2, num3)
+# min = min(num1, num2, num3)
+# mean = (num1 + num2 + num3)/3
+# if user_choice == 1:
+#     print(max)
+# elif user_choice == 2:
+#     print(min)
+# elif user_choice == 3:
+#     print(mean)
+# else:
+#     print("Incorrect user_choice")
+
